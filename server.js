c38b2dfce5defe06d7c271c09c1f6ae3df789411
Fasica Robi <fasicasr@gmail.com>
2021-05-02 00:30:32 -0400

    port change to 80

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const uniqid = require('uniqid');
 //const uniqid = require('uniqid');
 
 const app = express();
-const PORT = 80;
+const PORT = process.env.PORT || 3000;;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
